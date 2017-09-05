e65121bd1c0471e8473a737ab0bcbcc1ee8f9ff0
dhuangg <daniel.huang87@gmail.com>
2017-09-04 18:09:40 -0700

    added font, partially done with navbar

--- scripts.js
+++ scripts.js
@@ -1,3 +1,28 @@
 /**
  * Created by danielhuang on 9/4/17.
  */
+$("#con-button").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#concert").offset().top
+    }, 2000);
+});
+$("#film-button").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#film").offset().top
+    }, 2000);
+});
+$("#fes-button").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#festival").offset().top
+    }, 2000);
+});
+$("#about-cec-cac-button").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#about-cec-cac").offset().top
+    }, 2000);
+});
+$("#lot-button").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#lottery-FAQ").offset().top
+    }, 2000);
+});
